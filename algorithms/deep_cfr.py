91dd44127e629018e2fc5244d5dadeb513c1b28c
mwalton <walton.myke@gmail.com>
2021-02-16 23:56:04 +0100

    deepcfr solve loop

--- algorithms/deep_cfr.py
+++ algorithms/deep_cfr.py
@@ -24,28 +24,31 @@ flags.DEFINE_integer("batch_size_advantage", 128, "Adv fn batch size")
 flags.DEFINE_integer("batch_size_strategy", 1024, "Strategy batch size")
 flags.DEFINE_integer("num_hidden", 64, "Hidden units in each layer")
 flags.DEFINE_integer("num_layers", 3, "Depth of neural networks")
-flags.DEFINE_bool("reinitialize_advantage_networks", False, "Re-init value net on each CFR iter")
+flags.DEFINE_bool("reinitialize_advantage_networks", False,
+                  "Re-init value net on each CFR iter")
 flags.DEFINE_string("game", "leduc_poker", "Name of the game")
 flags.DEFINE_integer("players", 2, "Number of players")
 flags.DEFINE_integer("logfreq", 100, "How often to print the exploitability")
 flags.DEFINE_string("project", "openspiel", "project name")
 flags.DEFINE_boolean("no_wandb", False, "Disables Weights & Biases")
 
+
 def solve(self):
     """Modified deep-cfr solution logic for online policy evaluation"""
     advantage_losses = collections.defaultdict(list)
     for _ in range(self._num_iterations):
         for p in range(self._num_players):
-        for _ in range(self._num_traversals):
-            self._traverse_game_tree(self._root_node, p)
-        if self._reinitialize_advantage_networks:
-            # Re-initialize advantage network for player and train from scratch.
-            self.reinitialize_advantage_network(p)
-        advantage_losses[p].append(self._learn_advantage_network(p))
+            for _ in range(self._num_traversals):
+                self._traverse_game_tree(self._root_node, p)
+            if self._reinitialize_advantage_networks:
+                # Re-initialize advantage network for player and train from scratch.
+                self.reinitialize_advantage_network(p)
+            advantage_losses[p].append(self._learn_advantage_network(p))
         self._iteration += 1
     # Train policy network.
     policy_loss = self._learn_strategy_network()
     return self._policy_network, advantage_losses, policy_loss
+
 
 def main(argv):
     if not FLAGS.no_wandb:
@@ -53,15 +56,18 @@ def main(argv):
         wandb.init(project=FLAGS.project)
         wandb.config.update(flags.FLAGS)
         wandb.config.update({"solver": "deep cfr"})
-    
-    game = pyspiel.load_game(FLAGS.game, {"players": pyspiel.GameParameter(FLAGS.players)})
+
+    game = pyspiel.load_game(
+        FLAGS.game, {"players": pyspiel.GameParameter(FLAGS.players)})
 
     with tf.Session() as sess:
         deep_cfr_solver = deep_cfr.DeepCFRSolver(
             sess,
             game,
-            policy_network_layers=tuple([FLAGS.num_hidden for _ in range(FLAGS.num_layers)]),
-            advantage_network_layers=tuple([FLAGS.num_hidden for _ in range(FLAGS.num_layers)]),
+            policy_network_layers=tuple(
+                [FLAGS.num_hidden for _ in range(FLAGS.num_layers)]),
+            advantage_network_layers=tuple(
+                [FLAGS.num_hidden for _ in range(FLAGS.num_layers)]),
             num_iterations=FLAGS.logfreq,
             num_traversals=FLAGS.num_traversals,
             learning_rate=1e-3,
@@ -77,13 +83,16 @@ def main(argv):
 
         for i in range(outer_iter):
             _, advantage_losses, policy_loss = deep_cfr_solver.solve()
-            
-            average_policy = policy.tabular_policy_from_callable(game, deep_cfr_solver.action_probabilities)
+
+            average_policy = policy.tabular_policy_from_callable(
+                game, deep_cfr_solver.action_probabilities)
             conv = exploitability.nash_conv(game, average_policy)
             if not FLAGS.no_wandb:
                 wandb.log({"Iteration": i * FLAGS.logfreq, 'NashConv': conv})
 
-            logging.info("Iteration: {} NashConv: {}".format(i * FLAGS.logfreq, conv))
+            logging.info("Iteration: {} NashConv: {}".format(
+                i * FLAGS.logfreq, conv))
+
 
 if __name__ == "__main__":
     app.run(main)
--- run.py
+++ run.py
@@ -3,10 +3,14 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--cfg", default="experiment.yaml", help="Experiment config file")
-parser.add_argument("--jobdir", default="jobs", help="Where to store generated slurm jobfiles and logs")
-parser.add_argument("--local", dest='local', action='store_true', help="No cluster; Run in the current session")
-parser.add_argument("--no_wandb", dest='no_wandb', action='store_true', help="Don't log to Weights & Biases")
+parser.add_argument("--cfg", default="experiment.yaml",
+                    help="Experiment config file")
+parser.add_argument("--jobdir", default="jobs",
+                    help="Where to store generated slurm jobfiles and logs")
+parser.add_argument("--local", dest='local', action='store_true',
+                    help="No cluster; Run in the current session")
+parser.add_argument("--no_wandb", dest='no_wandb',
+                    action='store_true', help="Don't log to Weights & Biases")
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -16,23 +20,28 @@ if __name__ == "__main__":
     for experiment in config["run"]:
         exp_config = config[experiment]
         jobfile = os.path.join(args.jobdir, "{}.job".format(experiment))
-        jobargs = (config["container"], exp_config["script"], exp_config["flagfile"])
+        jobargs = (config["container"], exp_config["script"],
+                   exp_config["flagfile"])
         cmd = "singularity exec {} python3 {} --flagfile {}".format(*jobargs)
         if args.no_wandb:
             cmd = cmd + " --no_wandb"
-        
+
         with open(jobfile, "w") as f:
             f.writelines("#!/bin/bash\n")
-            f.writelines("#SBATCH --partition {}\n".format(exp_config["partition"]))
+            f.writelines(
+                "#SBATCH --partition {}\n".format(exp_config["partition"]))
             f.writelines("#SBATCH --nodes {}\n".format(exp_config["nodes"]))
-            f.writelines("#SBATCH --ntasks-per-node {}\n".format(exp_config["ntasks-per-node"]))
-            f.writelines("#SBATCH --mem-per-cpu {}\n".format(exp_config["mem-per-cpu"]))
+            f.writelines(
+                "#SBATCH --ntasks-per-node {}\n".format(exp_config["ntasks-per-node"]))
+            f.writelines(
+                "#SBATCH --mem-per-cpu {}\n".format(exp_config["mem-per-cpu"]))
             f.writelines("#SBATCH --time {}\n".format(exp_config["time"]))
-            f.writelines("#SBATCH --job-name {}\n".format(exp_config["job-name"]))
+            f.writelines(
+                "#SBATCH --job-name {}\n".format(exp_config["job-name"]))
             f.writelines("#SBATCH --output {}\n".format(exp_config["output"]))
             f.writelines("module purge\n")
             f.writelines(cmd)
-        
+
         if args.local:
             os.system("bash {}".format(jobfile))
         else:
